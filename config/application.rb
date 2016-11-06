3cccffb6697d3a5715086ca7291f59da9991f1ed
Matt Gaudio <mgaudio15@gmail.com>
2016-11-06 17:00:49 -0500

    changes

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,10 @@ Bundler.require(*Rails.groups)
 
 module MythicGamingApp
   class Application < Rails::Application
+
+    # Custom 
+    require 'susy'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
