818554dd96d21139fd5ab9dadd9d292e0061b1dc
Matt Gaudio <mgaudio15@gmail.com>
2016-12-17 21:23:55 -0500

    some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+game = Game.create(title: 'Halo:MCC', category: 'fps', active: true)
+bracket = game.brackets.create(title: 'Championship', team_size: '4', rounds: '3')
+map = bracket.maps.create(name: "Lockout")
+gametype = map.gametypes([ { name: "Oddball" }, { name: 'King of the Hill' }])
+
+game = Game.create(title: 'League of Legends', category: 'moba', active: true)
+bracket = game.brackets.create(title: 'Championship', team_size: '4', rounds: '1')
+map = bracket.maps.create(name: "Territory")
+gametype = map.gametypes([ { name: "Challenge" } ])
+
+
